90aa629600e69bb37d6c27029e8e30054e01694c
mubashar-mizA <mirzamubashar535@gmail.com>
2024-12-20 09:52:11 +0500

    changing the server

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 import express from "express";
 const app = express()
-// import dotenv from 'dotenv'
-// import cors from 'cors'
-// app.use(cors())
-// dotenv.config()
-// app.use(express.json())
-// import authRoutes from './routes/auth.routes.js'
-// import ConnectDB from "./utilits/connectDB.js";
 
-// const PORT = process.env.PORT || 3310
-// const DB_URL = process.env.DB_URL
-
-// ConnectDB(DB_URL)
 const PORT = 1110
 
 app.get('/ping', (req, res) => {
     res.send('Dirrty minds!')
 })
 
-// app.use('/user', authRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`)
